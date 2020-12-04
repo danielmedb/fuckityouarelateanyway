8a0196b5e2f857fa52c74236b86885bffa9eadbf
danielmedb <70508723+danielmedb@users.noreply.github.com>
2020-12-04 09:47:44 +0100

    updates

--- functions.js
+++ functions.js
@@ -10,41 +10,22 @@ function setNewTime(){
     return time;
 }
 
-let newTime = setNewTime();
 
-const body = document.querySelector('body');
-const main = document.querySelector('.main');
-
-document.addEventListener('scroll', () => {
-    body.style.background = '';
-    window.onscroll = function(ev) {
-        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-            const lastElementColor = document.querySelector('.endless') === null ? main.dataset.color : main.lastElementChild.dataset.color;                       
-            const endlessDiv = document.createElement('div');
-            endlessDiv.classList.add('endless');
-            const colors = random_bg_color(lastElementColor);
-            endlessDiv.style.setProperty('background', `linear-gradient(${colors.first}, ${colors.second})`);
-            endlessDiv.setAttribute('data-color', `${colors.second}`);
-            document.querySelector(".main").append(endlessDiv);            
-        }
-    };
-});
-
-document.addEventListener('click', () => {
-    alert('Fuck it! You\'re late anyway!');
-});
-
-document.addEventListener('keypress', () => {
-    // body.style.background = random_bg_color();
-});
-
-function random_bg_color(bgColor) {
+function random_bg_color(bgColor = null, second = null) {
     const a = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
     const c = Math.floor(Math.random() * 256);
+    
+    // set main as different colors every reload.
+    if(bgColor === null){
+        const x = Math.floor(Math.random() * 256);
+        const y = Math.floor(Math.random() * 256);
+        const z = Math.floor(Math.random() * 256);
+        bgColor = `rgb(${x}, ${y}, ${z})`;
+    }
+
 
     const nextbgColor = `rgb(${a}, ${b}, ${c})`;
-    
     const colors = {
         first : bgColor,
         second : nextbgColor
@@ -52,20 +33,39 @@ function random_bg_color(bgColor) {
     return colors;
 }
 
+function createEndlessDiv(scrollDirection){
 
+    const endlessDiv = document.createElement('div');
+    endlessDiv.classList.add('endless');
 
-setInterval(function() {
-   
-    newTime.sec = (newTime.sec === 59 ? 0 : newTime.sec + 1);
-    newTime.min = (newTime.sec === 0 ? newTime.min + 1 : newTime.min);
-    newTime.hour = (newTime.min === 0 ? newTime.hour + 1 : newTime.hour);
-   
-    setClock(newTime.hour, newTime.min, newTime.sec);
-}, 1000);
+    // Get the first div with class endless and its color. If it doesnt exists. Get the .main color.
+    const lastElementColor = document.querySelector('.endless') === null ? main.dataset.color : main.lastElementChild.dataset.color;  
+     
+    if(scrollDirection === 'Down'){
 
-const hour = document.querySelector(".hour"),
-min = document.querySelector(".min"),
-sec = document.querySelector(".sec");
+        // Lets get a random color for the next endless div.
+        scrollDownColors = random_bg_color(lastElementColor);
+
+        // Set background to linear-gradient
+        endlessDiv.style.setProperty('background', `linear-gradient(${scrollDownColors.first}, ${scrollDownColors.second})`);
+        endlessDiv.setAttribute('data-color', `${scrollDownColors.second}`);
+        endlessDiv.setAttribute('data-color-first', `${scrollDownColors.first}`);
+        main.append(endlessDiv);
+    }else{
+        const firstDivInScrollUp = document.querySelector('.clock').previousElementSibling;
+        const elementColor = document.querySelector('.endless').dataset.color;
+
+        scrollDownColors = random_bg_color(elementColor);
+        if(firstDivInScrollUp === null){
+            endlessDiv.style.setProperty('background', `linear-gradient(${scrollDownColors.first}, ${scrollDownColors.second})`);
+        }else{
+            endlessDiv.style.setProperty('background', `linear-gradient(${scrollDownColors.second}, ${scrollDownColors.first})`);
+        }
+        endlessDiv.setAttribute('data-color', `${scrollDownColors.second}`);
+        
+        main.prepend(endlessDiv);  
+    }
+}
 
 
 function setClock(newHour, newMin, newSec){
@@ -79,12 +79,26 @@ function setClock(newHour, newMin, newSec){
     const hours = 366 - ( newHour * 6 ) - 6;
 
     // set elements transform
-    setRotation(sec, seconds);  
-    setRotation(min, minutes);
-    setRotation(hour, hours);
+    setRotation(secElement, seconds);  
+    setRotation(minElement, minutes);
+    setRotation(hourElement, hours);
 }
 
 
 function setRotation(element, rotation){
     element.style.transform = `rotate(${rotation}deg`;
 }
+
+// Create clock number and rotate it.
+function createNumbers(){
+    const clock = document.querySelector('.clock');
+    for(let i = 1; i <= 12; i++){
+        const number = document.createElement('div');
+        number.classList.add('number', `number-${i}`);
+        number.textContent = i;
+        number.style.transform = `rotate(${i*30}deg)`;
+        clock.append(number);
+    }
+}
+
+createNumbers();
--- scripts.js
+++ scripts.js
@@ -1,14 +1,129 @@
-// create divs of numbers
 
-function createNumbers(){
-    const clock = document.querySelector('.clock');
-    for(let i = 1; i <= 12; i++){
-        const number = document.createElement('div');
-        number.classList.add('number', `number-${i}`);
-        number.textContent = i;
-        number.style.transform = `rotate(${i*30}deg)`;
-        clock.append(number);
+
+let newTime = setNewTime();
+
+const body = document.querySelector('body');
+const main = document.querySelector('.main');
+const text = document.querySelector('.zerofucksgiven');
+const clock = document.querySelector('.clock');
+
+
+const mainColor = random_bg_color();
+main.style.setProperty('background', `linear-gradient(${mainColor.first}, ${mainColor.second})`);
+
+
+
+for(let a = 0; a <= 5; a++){
+    if(a % 2){
+        createEndlessDiv('Up');
+    }else{
+        createEndlessDiv('Down');
     }
 }
 
-createNumbers();
+let scrollFromTop = 0;
+let direction = 0;
+let distance = 4;
+let usePlus = true;
+let scrollDirection = '';
+
+let math = {
+    '+' : function(x,y) {return x + y;},
+    '-' : function(x,y) {return x - y;}
+}
+
+window.addEventListener('scroll', () => {
+    let scrollDownColors;
+    // get first textW element
+    const firstTextwElement = document.querySelector('.textW');
+       
+    if((document.body.getBoundingClientRect()).top > scrollFromTop){
+        usePlus = false;
+        scrollDirection = 'Up';
+    }else{
+        usePlus = true;
+        scrollDirection = 'Down';
+    }
+    scrollFromTop = (document.body.getBoundingClientRect()).top;
+
+    // get every textW class.
+    const fuckItText = document.querySelectorAll(".textW");
+    [...fuckItText].forEach((element) => {
+        let currentRotation = parseInt(element.dataset.rotation);
+        element.setAttribute('data-rotation', math[usePlus ? '+' : '-'](currentRotation, distance));
+        element.style.transform = `rotate(${element.dataset.rotation}deg)`;
+    });
+    
+
+    const {scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    
+    // console.log(scrollTop);
+    if(clientHeight + scrollTop >= scrollHeight - 50 || scrollTop <= 450){
+
+        // If we scroll down(true) or up(false)!
+        console.log(scrollDirection);
+
+        // const lastElementColor = document.querySelector('.endless') === null ? main.dataset.color : main.lastElementChild.dataset.color;                       
+        // let endlessDiv = document.createElement('div');
+        
+        createEndlessDiv(scrollDirection);
+        
+        // if(scrollDown === false){
+        //     const firstElementColor = document.querySelector('.endless').dataset.color;
+        //     console.log(firstElementColor);
+        //     scrollDownColors = random_bg_color(firstElementColor, firstElementColor);
+        //     console.log(scrollDownColors);
+        //     endlessDiv.classList.add('endless');
+        //     endlessDiv.style.setProperty('background', `linear-gradient(${scrollDownColors.second}, ${scrollDownColors.first})`);
+        //     endlessDiv.setAttribute('data-color', `${scrollDownColors.second}`);
+        //     main.prepend(endlessDiv);  
+        // }else{
+        //     scrollDownColors = random_bg_color(lastElementColor);
+        //     endlessDiv.classList.add('endless');
+        //     endlessDiv.style.setProperty('background', `linear-gradient(${scrollDownColors.first}, ${scrollDownColors.second})`);
+        //     endlessDiv.setAttribute('data-color', `${scrollDownColors.second}`);
+        //     main.append(endlessDiv);   
+        // }
+    }
+});
+
+document.addEventListener('click', () => {
+    const fuckItText = text.textContent;
+
+});
+
+document.addEventListener('keypress', () => {
+    // body.style.background = random_bg_color();
+});
+
+
+// Clone newTime if we want to use newTime somewhere else.
+let time = {...newTime}
+setInterval(function() {
+    
+    time.sec = (time.sec === 59 ? 0 : time.sec + 1);
+    time.min = (time.sec === 0 ? time.min + 1 : time.min);
+    time.hour = (time.min === 0 ? time.hour + 1 : time.hour);
+    
+   
+    setClock(time.hour, time.min, time.sec);
+}, 1000);
+
+// Get hour, min and sec divs.
+const hourElement = document.querySelector(".hour");
+const minElement = document.querySelector(".min");
+const secElement = document.querySelector(".sec");
+
+[...text.textContent].forEach((i) => {
+    
+    const countDivs = document.querySelectorAll('div.textW').length;
+
+    const rotationPerCharacter = (180 / text.textContent.length).toFixed();
+
+    let span = document.createElement('div');
+    span.classList.add('textW');
+    span.setAttribute('data-rotation', (rotationPerCharacter * countDivs));
+    span.textContent = i;
+    span.style.transform = `rotate(${rotationPerCharacter * countDivs}deg)`;
+    clock.append(span);
+});
